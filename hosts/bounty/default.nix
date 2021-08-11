a050a29497f75f6e9834a1d3102d037c653355b4
Kike Fontán <26680023+CosasDePuma@users.noreply.github.com>
2021-08-11 04:46:12 +0200

    :zap: Better dotfiles management

--- hosts/bounty/default.nix
+++ hosts/bounty/default.nix
@@ -2,14 +2,6 @@
 let
   user = "bug";
 in {
-  imports = [
-    ../../modules/i3.nix
-    ../../modules/hacking.nix
-  ];
-
-  # System
-  networking.hostName = "bounty";
-
   # User
   users.users."${user}" = {
     uid = 1000;
@@ -20,4 +12,29 @@ in {
     extraGroups = [ "network" "wheel" "wireshark" ];
   };
   services.xserver.displayManager.autoLogin.user = "${user}";
+
+  home-manager.users."${user}" = { pkgs, ... }: {
+    # Programs
+    home.packages = with pkgs; [
+      amass        # dns enumeration
+      burpsuite    # web security platform
+      ffuf         # bruteforce assets
+      findomain    # subdomain discovery
+      gau          # known urls
+      gobuster     # bruteforce assets
+      httpx        # http toolkit
+      metasploit   # metasploit xdd
+      naabu        # syn/connect port scanner
+      nmap         # port scanner
+      nuclei       # targeted scanning
+      sqlmap       # automatic sql injection
+      subfinder    # subdomain discovery
+      wireshark    # network protocol analyzer
+      wpscan       # wordpress
+      zap          # web security platform (open-source)
+    ];
+
+    # Configuration
+    xsession.windowManager.i3 = import ../../config/i3.nix pkgs; 
+  };
 }
