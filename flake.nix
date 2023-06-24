8d2584f3a4ffe1da394a5522fd16fd87961bc8e0
Kike Fontan <hola+git@kike.wtf>
2023-06-24 07:17:18 +0200

    ✨ Much better Flake!

--- flake.nix
+++ flake.nix
@@ -2,30 +2,35 @@
   description = "Pumita's dotfiles";
 
   outputs = { self, nixpkgs }:
-    let
-      name = "dotfiles";
-      forAllSystems = with nixpkgs.lib; genAttrs system.flakeExposed;
+    let name = "dotfiles";
     in {
-      packages = forAllSystems (system:
-        let pkgs = import nixpkgs { inherit system; }; in {
-          default = self.packages.${system}."${name}";
-          "${name}" = pkgs.stdenvNoCC.mkDerivation {
-            inherit name;
-            src = ./.;
-
-            dontBuild = true;
-            dontConfigure = true;
-            buildInputs = with pkgs; [ rsync ];
-
-            installPhase = ''
-              rsync -gortux --no-p    \
-                --exclude ".git*"     \
-                --exclude "flake.*"   \
-                --exclude "LICENSE"   \
-                --exclude "README.md" \
-                ./ "$out"
-            '';
+      homeManagerModules.default = { config, lib, pkgs, ... }:
+        let
+          cfg = config."${name}";
+          dotfile = path:
+            with lib.strings; { "${path}".text = removePrefix "\n" (removeSuffix "\n" (builtins.readFile ./.config/${path})); };
+       in {
+          options."${name}" = {
+            curl = lib.mkEnableOption "cURL dotfiles";
+            git  = lib.mkEnableOption "git dotfiles";
+            ssh  = lib.mkEnableOption "SSH dotfiles";
+            wget = lib.mkEnableOption "wget dotfiles";
           };
-        });
+
+          config =
+            lib.mkIf cfg.curl {
+              home.packages = with pkgs; [ curl ];
+              xdg.configFile = dotfile "../.curlrc";
+            } // lib.mkIf cfg.git {
+              programs.git.enable = true;
+              xdg.configFile = dotfile "git/config" // dotfile "git/ignore";
+            } // lib.mkIf cfg.ssh {
+              programs.ssh.enable = true;
+              xdg.configFile = dotfile "ssh/config";
+            } // lib.mkIf cfg.wget {
+              home.packages = with pkgs; [ wget ];
+              xdg.configFile = dotfile "../.wgetrc";
+            };
+        };
     };
 }
