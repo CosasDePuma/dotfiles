42c8cbf51d3ae1af8c71a992ef8ac567906a2b71
Kike <hola+git@kike.wtf>
2023-07-05 22:42:21 +0200

    🚀

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,5 +1,26 @@
 import XMonad
 import XMonad.Config (def)
+import XMonad.Util.EZConfig (additionalKeysP)
 
 main :: IO ()
-main = xmonad def
+main = xmonad $ def {
+    -- -----------------
+    --  Variables
+    -- -----------------
+    terminal = "kitty",
+    modMask = mod4Mask,
+    altMask = mod1Mask
+  } `additionalKeysP` [
+    -- -----------------
+    --  Keybindings
+    -- -----------------
+    -- XMonad
+    ("M-S-q",       spawn "xmonad --recompile && xmonad --restart"),
+    -- Programs
+    ("M-b",         spawn "firefox"),
+    ("M-S-b",       spawn "firefox --private-window"),
+    ("M-e",         spawn "thunar"),
+    ("M-<Return>",  spawn "kitty"),
+    ("M-S-<Space>", spawn "rofi -show drun"),
+    ("M-S-<Tab>",   spawn "rofi -show window")
+  ]
