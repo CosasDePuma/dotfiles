7a6d04b134deed27a041168d9b6c074af504c23a
Kike <hola+git@kike.wtf>
2023-07-06 23:25:49 +0200

    🎨

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
         
         cpyFile = pkg: path: {
           "${pkg}" = lib.hm.dag.entryAfter ["writeBoundary"] ''
-            if ${pkgs.coreutils}/bin/test -x "${sw-profile}/${pkg}" || ${pkgs.coreutils}/bin/test -x "${sw-system}/${pkg}"; then
+            if ${pkgs.coreutils}/bin/test -x ${sw-profile}/"${pkg}" || ${pkgs.coreutils}/bin/test -x ${sw-system}/"${pkg}"; then
               $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/${path} ~/${path}
               $DRY_RUN_CMD ${pkgs.coreutils}/bin/chmod $VERBOSE_ARG ug+rw,o-rwx ~/${path}
             fi
@@ -18,7 +18,7 @@
         };
         cpyFolder = pkg: path: {
           "${pkg}" = lib.hm.dag.entryAfter ["writeBoundary"] ''
-            if ${pkgs.coreutils}/bin/test -x "${sw-profile}/${pkg}" || ${pkgs.coreutils}/bin/test -x "${sw-system}/${pkg}"; then
+            if ${pkgs.coreutils}/bin/test -x ${sw-profile}/"${pkg}" || ${pkgs.coreutils}/bin/test -x ${sw-system}/"${pkg}"; then
               $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir $VERBOSE_ARG -p ~/${path}
               $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/${path}/. ~/${path}
               $DRY_RUN_CMD ${pkgs.coreutils}/bin/chmod $VERBOSE_ARG -R ug+rw,o-rwx ~/${path}
@@ -41,11 +41,11 @@
                 $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir $VERBOSE_ARG -p ~/.local/bin/
                 $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/.local/bin/theme ~/.local/bin/theme
 
-                if ${pkgs.coreutils}/bin/test -x "${sw-profile}/feh" || ${pkgs.coreutils}/bin/test -x "${sw-system}/feh"; then
+                if ${pkgs.coreutils}/bin/test -x ${sw-profile}/"feh" || ${pkgs.coreutils}/bin/test -x ${sw-system}/"feh"; then
                   $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/.local/bin/feh ~/.local/bin/feh
                 fi
 
-                if ${pkgs.coreutils}/bin/test -x "${sw-profile}/swww" || ${pkgs.coreutils}/bin/test -x "${sw-system}/swww"; then
+                if ${pkgs.coreutils}/bin/test -x ${sw-profile}/"swww" || ${pkgs.coreutils}/bin/test -x ${sw-system}/"swww"; then
                   $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/.local/bin/swww ~/.local/bin/swww
                 fi
 
