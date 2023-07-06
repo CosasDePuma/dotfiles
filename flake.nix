163d2ec387f154fbf8558b00875316054fe0e06c
Kike <hola+git@kike.wtf>
2023-07-06 19:26:59 +0200

    📝

--- flake.nix
+++ flake.nix
@@ -10,14 +10,29 @@
 
         config = mkIf cfg.enable {
           home.activation = {
+            # cURL
+            curl = lib.hm.dag.entryAfter ["writeBoundary"] ''
+              command -v "curl" > /dev/null && $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.}/.curlrc ~/.curlrc
+            '';
             # Local scripts
-            bin = lib.hm.dag.entryAfter ["writeBoundary"] ''
-              $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.local/bin}/. ~/.local/bin
+            local = lib.hm.dag.entryAfter ["writeBoundary"] ''
+              $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.local/bin}/theme ~/.local/bin/theme
+              command -v "feh"  >/dev/null && $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.local/bin}/feh  ~/.local/bin/feh
+              command -v "swww" >/dev/null && $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.local/bin}/swww ~/.local/bin/swww
+              $DRY_RUN_CMD chmod -R +x ~/.local/bin/
             '';
             # SSH
-            #"ssh" = lib.hm.dag.entryAfter ["writeBoundary"] ''
-            #  command -v "ssh" > /dev/null && $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.ssh} ~/.ssh
-            #'';
+            ssh = lib.hm.dag.entryAfter ["writeBoundary"] ''
+              command -v "ssh" > /dev/null && $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.ssh}/. ~/.ssh/
+            '';
+            # Wget
+            wget = lib.hm.dag.entryAfter ["writeBoundary"] ''
+              command -v "wget" > /dev/null && $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.}/.wgetrc ~/.wgetrc
+            '';
+            # XMonad
+            xmonad = lib.hm.dag.entryAfter ["writeBoundary"] ''
+              command -v "xmonad" > /dev/null && $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.config/xmonad}/. ~/.config/xmonad/
+            '';
           };
         };
       };
