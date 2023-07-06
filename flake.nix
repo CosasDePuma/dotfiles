28ad83a0b71b33278d1b31522f32ea301332b02c
Kike <hola+git@kike.wtf>
2023-07-06 23:22:11 +0200

    🔥

--- flake.nix
+++ flake.nix
@@ -5,62 +5,51 @@
     homeManagerModules.default = { config, lib, pkgs, ... }:
       let
         cfg = config.dotfiles;
+        sw-profile = "~/.nix-profile/bin";
+        sw-system = "/run/current-system/sw/bin";
+        
+        cpyFile = pkg: path: lib.hm.dag.entryAfter ["writeBoundary"] ''
+          if ${pkgs.coreutils}/bin/test -x "${sw-profile}/${pkg}" || ${pkgs.coreutils}/bin/test -x "${sw-system}/${pkg}"; then
+            $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/${path} ~/${path}
+            $DRY_RUN_CMD ${pkgs.coreutils}/bin/chmod $VERBOSE_ARG ug+rw,o-rwx ~/${path}
+          fi
+        '';
+        cpyFolder = pkg: path: lib.hm.dag.entryAfter ["writeBoundary"] ''
+          if ${pkgs.coreutils}/bin/test -x "${sw-profile}/${pkg}" || ${pkgs.coreutils}/bin/test -x "${sw-system}/${pkg}"; then
+            $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir $VERBOSE_ARG -p ~/${path}
+            $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/${path}/. ~/${path}
+            $DRY_RUN_CMD ${pkgs.coreutils}/bin/chmod $VERBOSE_ARG -R ug+rw,o-rwx ~/${path}
+          fi
+        '';
+
       in with lib; {
         options.dotfiles.enable = mkEnableOption "dotfiles";
 
         config = mkIf cfg.enable {
-          home.activation =
-           let
-              path = "/run/current-system/sw/bin";
-           in {
-            # cURL
-            curl = lib.hm.dag.entryAfter ["writeBoundary"] ''
-              if ${pkgs.coreutils}/bin/test -x "${path}/curl"; then
-                $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/.curlrc ~/.curlrc
-                $DRY_RUN_CMD ${pkgs.coreutils}/bin/chmod $VERBOSE_ARG -R ug+rw,o-rwx ~/.curlrc
-              fi
-            '';
-            # SSH
-            ssh = lib.hm.dag.entryAfter ["writeBoundary"] ''
-              if ${pkgs.coreutils}/bin/test -x "${path}/ssh"; then
-                $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir $VERBOSE_ARG -p ~/.ssh/
-                $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/.ssh/. ~/.ssh
-                $DRY_RUN_CMD ${pkgs.coreutils}/bin/chmod $VERBOSE_ARG -R ug+rw,o-rwx ~/.ssh/
-              fi
-            '';
-            # Wget
-            wget = lib.hm.dag.entryAfter ["writeBoundary"] ''
-              if ${pkgs.coreutils}/bin/test -x "${path}/wget"; then
-                $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/.wgetrc ~/.wgetrc
-                $DRY_RUN_CMD ${pkgs.coreutils}/bin/chmod $VERBOSE_ARG -R ug+rw,o-rwx ~/.wgetrc
-              fi
-            '';
-            # XMonad
-            xmonad = lib.hm.dag.entryAfter ["writeBoundary"] ''
-              if ${pkgs.coreutils}/bin/test -x "${path}/xmonad"; then
-                $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir $VERBOSE_ARG -p ~/.config/xmonad/
-                $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/.config/xmonad/. ~/.config/xmonad
-                $DRY_RUN_CMD ${pkgs.coreutils}/bin/chmod $VERBOSE_ARG -R ug+rw,o-rwx ~/.config/xmonad/
-              fi
-            '';
+          home.activation = mkMerge [
+            (cpyFile   "curl"     ".curlrc"       )
+            (cpyFolder "ssh"      ".ssh"          )
+            (cpyFile   "wget"     ".wgetrc"       )
+            (cpyFolder "xmonad"   ".config/xmonad")
 
-            # Local scripts
-            local = lib.hm.dag.entryAfter ["writeBoundary"] ''
-              $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir $VERBOSE_ARG -p ~/.local/bin/
-              $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.local/bin}/theme ~/.local/bin/theme
+            ({
+              local-scripts = lib.hm.dag.entryAfter ["writeBoundary"] ''
+                echo "Installing local scripts from ${./.}..."
+                $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir $VERBOSE_ARG -p ~/.local/bin/
+                $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/.local/bin/theme ~/.local/bin/theme
 
-              # Feh
-              if ${pkgs.coreutils}/bin/test -x "${path}/feh"; then
-                $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.local/bin}/feh ~/.local/bin/feh
-              fi
+                if ${pkgs.coreutils}/bin/test -x "${sw-profile}/feh" || ${pkgs.coreutils}/bin/test -x "${sw-system}/feh"; then
+                  $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/.local/bin/feh ~/.local/bin/feh
+                fi
 
-              # Swww
-              if ${pkgs.coreutils}/bin/test -x "${path}/swww"; then
-                $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.local/bin}/swww ~/.local/bin/swww
-              fi
-              $DRY_RUN_CMD ${pkgs.coreutils}/bin/chmod $VERBOSE_ARG -R ug+rwx,o-x ~/.local/bin/
-            '';
-          };
+                if ${pkgs.coreutils}/bin/test -x "${sw-profile}/swww" || ${pkgs.coreutils}/bin/test -x "${sw-system}/swww"; then
+                  $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/.local/bin/swww ~/.local/bin/swww
+                fi
+
+                $DRY_RUN_CMD ${pkgs.coreutils}/bin/chmod $VERBOSE_ARG -R ug+rwx,o-x ~/.local/bin/
+              '';
+            })
+          ];
         };
       };
   };
