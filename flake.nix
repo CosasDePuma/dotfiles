a54751f1d1e2c469700683794bf9685b2222eb99
Kike <hola+git@kike.wtf>
2023-07-06 19:31:28 +0200

    🚀

--- flake.nix
+++ flake.nix
@@ -12,27 +12,29 @@
           home.activation = {
             # cURL
             curl = lib.hm.dag.entryAfter ["writeBoundary"] ''
-              command -v "curl" > /dev/null && $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.}/.curlrc ~/.curlrc
+              command -v "curl" > /dev/null && ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.}/.curlrc ~/.curlrc
             '';
             # Local scripts
             local = lib.hm.dag.entryAfter ["writeBoundary"] ''
-              $DRY_RUN_CMD mkdir -p ~/.local/bin
-              $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.local/bin}/theme ~/.local/bin/theme
-              command -v "feh"  >/dev/null && $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.local/bin}/feh  ~/.local/bin/feh
-              command -v "swww" >/dev/null && $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.local/bin}/swww ~/.local/bin/swww
-              $DRY_RUN_CMD chmod -R +x ~/.local/bin/
+              mkdir -p ~/.local/bin
+              ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.local/bin}/theme ~/.local/bin/theme
+              command -v "feh"  >/dev/null && ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.local/bin}/feh  ~/.local/bin/feh
+              command -v "swww" >/dev/null && ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.local/bin}/swww ~/.local/bin/swww
+              chmod -R +x ~/.local/bin/
             '';
             # SSH
             ssh = lib.hm.dag.entryAfter ["writeBoundary"] ''
-              command -v "ssh" > /dev/null && $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.ssh}/. ~/.ssh/
+              mkdir -p ~/.ssh
+              command -v "ssh" > /dev/null && ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.ssh}/. ~/.ssh/
             '';
             # Wget
             wget = lib.hm.dag.entryAfter ["writeBoundary"] ''
-              command -v "wget" > /dev/null && $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.}/.wgetrc ~/.wgetrc
+              command -v "wget" > /dev/null && ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.}/.wgetrc ~/.wgetrc
             '';
             # XMonad
             xmonad = lib.hm.dag.entryAfter ["writeBoundary"] ''
-              command -v "xmonad" > /dev/null && $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.config/xmonad}/. ~/.config/xmonad/
+              mkdir -p ~/.config/xmonad
+              command -v "xmonad" > /dev/null && ${pkgs.rsync}/bin/rsync -gortuxv --no-p ${./.config/xmonad}/. ~/.config/xmonad/
             '';
           };
         };
