9c7863d4d02de72b4482c67eade917b7d9c5d700
Kike <hola+git@kike.wtf>
2023-07-06 18:22:57 +0200

    🚀 Trying new approach

--- flake.nix
+++ flake.nix
@@ -5,120 +5,24 @@
       homeManagerModules.default = { config, lib, pkgs, ... }:
         let
           cfg = config.dotfiles;
-          binfile   = path: writeFile path path true;
-          dotfile   = path: writeFile path path false;
-          dotconfig = path: writeFile ".config/${path}" path false;
-          
-          writeFile = src: dest: x: with lib.strings; {
-            "${dest}" = {
-              text = removePrefix "\n" (removeSuffix "\n" (builtins.readFile ./${src}));
-              executable = x;
-            };
-          };
+
+          cpy = command: path: lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+            if command -v "${program}" > /dev/null || test -z "${program}"; then
+              $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortux --no-p "${path}" ~/"${path}"
+            fi
+
+            test "${program}" == "sh" && $DRY_RUN_CMD chmod -R +x ~/"${path}"
+          '';
        in with lib; {
-          options.dotfiles = {
-            all        = mkEnableOption "all the dotfiles";
-            curl       = mkEnableOption "cURL dotfiles";
-            feh        = mkEnableOption "feh dotfiles";
-            foot       = mkEnableOption "foot dotfiles";
-            git        = mkEnableOption "git dotfiles";
-            hyprland   = mkEnableOption "Hyprland dotfiles";
-            neofetch   = mkEnableOption "neofetch dotfiles";
-            ssh        = mkEnableOption "SSH dotfiles";
-            swww       = mkEnableOption "swww dotfiles";
-            wallpapers = mkEnableOption "wallpapers";
-            wget       = mkEnableOption "wget dotfiles";
+          options.dotfiles.enable = mkEnableOption "dotfiles";
 
-            themes = {
-              enable     = mkEnableOption "themes";
-              catppuccin = mkEnableOption "catppuccin theme";
-            };
+          config = mkIf cfg.enable {
+            home.activation = mkMerge [
+              (cpy "sh"     ./.local/bin)
+              (cpy "ssh"    ./.ssh)
+              (cpy "xmonad" ./.config/xmonad)
+            ]
           };
-
-          config = mkMerge [
-            # --------------------
-            #   System utils
-            # --------------------
-
-            # cURL
-            (mkIf (cfg.all || cfg.curl) {
-              home.packages = with pkgs; [ curl ];
-              home.file = dotfile ".curlrc";
-            })
-            # Git
-            (mkIf (cfg.all || cfg.git) {
-              programs.git.enable = mkDefault false;
-              home.packages = with pkgs; [ git ];
-              xdg.configFile = (dotconfig "git/config") //
-                               (dotconfig "git/ignore");
-            })
-            # Neofetch
-            (mkIf (cfg.all || cfg.neofetch) {
-              home.packages = with pkgs; [ neofetch ];
-              xdg.configFile = dotconfig "neofetch/config.conf";
-            })   
-            # SSH
-            (mkIf (cfg.all || cfg.ssh) {
-              home.file = dotfile ".ssh/config";
-            })            
-            # Wget
-            (mkIf (cfg.all || cfg.wget) {
-              home.packages = with pkgs; [ wget ];
-              home.file = dotfile ".wgetrc";
-            })
-
-            # --------------------
-            #   WM/DE utils
-            # --------------------
-
-            # Feh
-            (mkIf (cfg.all || cfg.feh) {
-              home.packages = with pkgs; [ feh ];
-              home.file = binfile ".local/bin/feh";
-            })
-            # Foot
-            (mkIf (cfg.all || cfg.git) {
-              programs.git.enable = mkDefault false;
-              home.packages = with pkgs; [ foot ];
-              xdg.configFile = dotconfig "foot/foot.ini";
-            })
-            # Hyprland
-            (mkIf (cfg.all || cfg.hyprland) {
-              xdg.configFile = (dotconfig "hypr/hyprland.conf") //
-                               (dotconfig "hypr/bindings.conf");
-            })
-            # Swww
-            (mkIf (cfg.all || cfg.swww) {
-              home.packages = with pkgs; [ swww ];
-              home.file = binfile ".local/bin/swww";
-            })
-
-            # --------------------
-            #   Themes
-            # --------------------
-            (mkIf (cfg.all || cfg.themes.enable) {
-              home.file = binfile ".local/bin/theme";
-              home.shellAliases."theme" = "~/.local/bin/theme ";
-            })
-
-            # catppuccin
-            (mkIf (cfg.all || cfg.themes.catppuccin) {
-              xdg.configFile."themes/catppuccin" = {
-                recursive = true;
-                source = .config/themes/catppuccin;
-              };
-            })
-
-            # --------------------
-            #   Wallpapers
-            # --------------------
-            (mkIf (cfg.all || cfg.wallpapers) {
-              xdg.configFile."wallpapers" = {
-                recursive = true;
-                source = .config/wallpapers;
-              };
-            })
-          ];
         };
     };
 }
