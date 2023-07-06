a52e499fa601c87ebc4bdb761c630312c2313e40
Kike <hola+git@kike.wtf>
2023-07-06 18:37:43 +0200

    🚀

--- flake.nix
+++ flake.nix
@@ -2,30 +2,23 @@
   description = "Pumita's dotfiles";
 
   outputs = { self, nixpkgs }: {
-      homeManagerModules.default = { config, lib, pkgs, ... }:
-        let
-          cfg = config.dotfiles;
+    homeManagerModules.default = { config, lib, pkgs, ... }:
+      let
+        cfg = config.dotfiles;
+      in with lib; {
+        options.dotfiles.enable = mkEnableOption "dotfiles";
 
-          cpy = program: path: {
-            "${program}" = lib.hm.dag.entryAfter ["writeBoundary"] ''
-              # copy dotfiles to a directory
-              if command -v "${program}" > /dev/null || test -z "${program}"; then
-                $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortux --no-p "${path}" ~/"${path}"
-              fi
-              # make scripts executable
-              test "${program}" == "sh" && $DRY_RUN_CMD chmod -R +x ~/"${path}"
+        config = mkIf cfg.enable {
+          home.activation = {
+            "bin" = lib.hm.dag.entryAfter ["writeBoundary"] ''
+              $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortux --no-p ${./.local/bin} ~/.local/bin
+            '';
+
+            "ssh" = lib.hm.dag.entryAfter ["writeBoundary"] ''
+              command -v "ssh" > /dev/null && $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync -gortux --no-p ${./.ssh} ~/.ssh
             '';
           };
-       in with lib; {
-          options.dotfiles.enable = mkEnableOption "dotfiles";
-
-          config = mkIf cfg.enable {
-            home.activation = mkMerge [
-              (cpy "sh"     "./.local/bin")
-              (cpy "ssh"    "./.ssh")
-              (cpy "xmonad" "./.config/xmonad")
-            ];
-          };
         };
-    };
+      };
+  }
 }
