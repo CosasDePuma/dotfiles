3490d041716f9ad10af18eeb22380ee282c5bd27
Kike <hola+git@kike.wtf>
2023-07-16 02:24:41 +0200

    💄

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
         dotFile = pkg: path: {
           "${pkg}" = lib.hm.dag.entryAfter ["writeBoundary"] ''
             if ${pkgs.coreutils}/bin/test -x ${sw-system}/"${pkg}"; then
-              $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/${path} ~/${path}
+              $DRY_RUN_CMD ${pkgs.coreutils}/bin/cp $VERBOSE_ARG -f ${./.}/${path} ~/${path}
               $DRY_RUN_CMD ${pkgs.coreutils}/bin/chmod $VERBOSE_ARG ug+rw,o-rwx ~/${path}
             fi
           '';
@@ -19,7 +19,7 @@
           "${pkg}" = lib.hm.dag.entryAfter ["writeBoundary"] ''
             if ${pkgs.coreutils}/bin/test -x ${sw-system}/"${pkg}"; then
               $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir $VERBOSE_ARG -p ~/${path}
-              $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/${path}/. ~/${path}
+              $DRY_RUN_CMD ${pkgs.coreutils}/bin/cp $VERBOSE_ARG -rf ${./.}/${path} ~/${path}
               $DRY_RUN_CMD ${pkgs.coreutils}/bin/chmod $VERBOSE_ARG -R ug+rw,o-rwx ~/${path}
             fi
           '';
@@ -27,7 +27,7 @@
         cpyFolder = name: path: {
           "${name}" = lib.hm.dag.entryAfter ["writeBoundary"] ''
             $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir $VERBOSE_ARG -p ~/${path}
-            $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/${path}/. ~/${path}
+            $DRY_RUN_CMD ${pkgs.coreutils}/bin/cp $VERBOSE_ARG -rf ${./.}/${path} ~/${path}
             $DRY_RUN_CMD ${pkgs.coreutils}/bin/chmod $VERBOSE_ARG -R ug+rw,o-rwx ~/${path}
           '';
         };
@@ -52,14 +52,14 @@
               ({
                 local-scripts = hm.dag.entryAfter ["writeBoundary"] ''
                   $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir $VERBOSE_ARG -p ~/.local/bin/
-                  $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/.local/bin/theme ~/.local/bin/theme
+                  $DRY_RUN_CMD ${pkgs.coreutils}/bin/cp $VERBOSE_ARG -f ${./.}/.local/bin/theme ~/.local/bin/theme
 
                   if ${pkgs.coreutils}/bin/test -x ${sw-system}/feh; then
-                    $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/.local/bin/feh ~/.local/bin/feh
+                    $DRY_RUN_CMD ${pkgs.coreutils}/bin/cp $VERBOSE_ARG -f ${./.}/.local/bin/feh ~/.local/bin/feh
                   fi
 
                   if ${pkgs.coreutils}/bin/test -x ${sw-system}/swww; then
-                    $DRY_RUN_CMD ${pkgs.rsync}/bin/rsync $VERBOSE_ARG -gortux --no-p ${./.}/.local/bin/swww ~/.local/bin/swww
+                    $DRY_RUN_CMD ${pkgs.coreutils}/bin/cp $VERBOSE_ARG -f ${./.}/.local/bin/swww ~/.local/bin/swww
                   fi
 
                   $DRY_RUN_CMD ${pkgs.coreutils}/bin/chmod $VERBOSE_ARG -R ug+rwx,o-x ~/.local/bin/
